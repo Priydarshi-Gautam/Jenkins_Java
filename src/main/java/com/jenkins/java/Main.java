b2f303ce9af0394fc821de747342b8789c56ae54
Priydarshi Gautam <priydarshigautam1@gmail.com>
2024-02-29 13:08:34 +0530

    hello jenkins

--- src/main/java/com/jenkins/java/Main.java
+++ src/main/java/com/jenkins/java/Main.java
@@ -3,5 +3,6 @@ package com.jenkins.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello jenkins users");
     }
 }
